1cfe61c31ced0219dd361287bc5d83d14beee5f5
Injamam I <injamam@Injamams-MacBook-Air.local>
2025-11-15 22:50:40 +0530

    Initial commit

---
+++ Hello.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+
+        /*System.out.println("HELLO World !!");
+        System.out.println("✅ Java is working perfectly on Mac!");*/
+
+        /* 
+        Scanner sc = new Scanner(System.in);  //Defining a Scanner as sc
+        System.out.println("Enter your name : ");
+        String name = sc.nextLine(); //Getting input as string in sc 
+        System.out.println("hello" +" "+ name + " "+"!!!!"); //Printing Output
+        */
+
+        
+
+
+    }
+
+
+}
