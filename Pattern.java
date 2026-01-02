8ac43fad47af58999ce9ad43781b87efc3aa6d09
Injamam I <injamam@Injamams-MacBook-Air.local>
2026-01-02 21:50:54 +0530

    Modified Pattern

--- Pattern.java
+++ Pattern.java
@@ -27,16 +27,56 @@ class Pattern {
         }
     }
 
+    public void pattern4(int n){
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(i);
+            }
+        System.out.println();
+        }
+    }
+
+    public void pattern5(int n){
+        for(int i = 0; i < n; i++){
+            for(int j = 1; j < n-i+1; j++){
+                System.out.print("* ");
+            }
+        System.out.println();
+        }
+    }
+    
+    public void pattern6(int n){
+        for(int i = 0; i < n; i++){
+            for(int j = 1; j < n-i+1; j++){
+                System.out.print(j);
+            }
+        System.out.println();
+        }
+    }
+    
+    public void pattern7(int n){
+        for(int i = 0; i < n; i++){
+            //space
+            for(int j = 0; j < n-i-1; j++){
+                System.out.print(" ");
+            }
+            //star
+            for(int j = 0;j < 2*i+1; j++){
+                System.out.print("*");
+            }
+            //space
+            for(int j = 0; j < n-i-1; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
     public static void main (String[]args){
 
     Pattern sol = new Pattern();
     int n = 5;
-    sol.pattern1(n);
+    sol.pattern7(n);
     System.out.println();
-    sol.pattern2(n);
-    System.out.println();
-    sol.pattern3(n);
-
     }
 
 }
