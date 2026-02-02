19b5e5d5bf1a574143e926ad15203f19d86a93bd
Injamam <injamami85@gmail.com>
2026-02-02 23:37:11 +0530

    Added more basic math functions

--- BasicMath.java
+++ BasicMath.java
@@ -17,6 +17,7 @@ public class BasicMath {
 
         return reverse == original;
     }
+    
     public int reversenum(int x){
         int rev = 0;
         while(x > 0){
@@ -26,6 +27,7 @@ public class BasicMath {
         }
         return rev;
     }
+    
     public int countdigits(int x){
 
         int count = 0;
@@ -35,6 +37,7 @@ public class BasicMath {
         }
         return count;
     }
+    
     public String armstringnum(int x){
         int original = x;
         int sum = 0;
@@ -50,6 +53,7 @@ public class BasicMath {
         else return "Not An Armstring Number";
 
     }
+    
     public int gcd(int a, int b){
 
         while (b != 0){
@@ -60,11 +64,80 @@ public class BasicMath {
         return a; // b becomes 0 and a is the gcd
     }
 
+    public boolean isprime(int n){
+        // Numbers less than or equal to 1 are not prime
+        if(n <= 1) return false;
+
+        // Check divisibility from 2 to sqrt(n)
+        for(int i = 2; i*i <= n; i++){ 
+
+        // If n is divisible by i, it is not prime
+            if(n % i == 0){
+                return false;
+            }
+        }
+        // If no divisor is found, n is prime
+        return true;
+    }
+
+    public static void FactorsOfNum(int n){
+        for(int i = 1; i*i <= n; i++){
+            if(n % i == 0){
+                System.out.println(i);
+            }
+            if(i != n / i){
+                System.out.println(n / i);
+            }
+        }
+    }
+ 
+    public int sumofdigits(int n){
+        
+        int sum = 0;
+
+        while( n > 0){
+            sum = sum + n % 10;
+            n = n / 10;
+        }
+        return sum;
+
+    }
+
+    
+    public static int productofdigit(int n){
+
+        int product = 1;
+
+        while(n>0){
+            product *= n%10;
+            n /= 10;
+        }
+        return product;
+    }
+
+    public static void EvenOddCount(int n){
+        int odd = 0;
+        int even = 0;
+
+        while (n>0){
+            if(n%2 == 0)
+                even++;
+            else
+                odd++;
+
+            n = n /10;
+        }
+        System.out.println("Odd Number :" + odd);
+        System.out.println("Even Number :" + even);
+
+    }
     public static void main (String args[]){
 
         Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
-        int y = sc.nextInt();
+        //int x = sc.nextInt();
+        //int y = sc.nextInt();
+        System.out.print("Enter the digits to multipile : ");
+        int n = sc.nextInt();
 
         BasicMath obj = new BasicMath();   // object creation
 
@@ -73,6 +146,13 @@ public class BasicMath {
         //System.out.println(obj.countdigits(x));
         //System.out.println(obj.armstringnum(x));
         //System.out.println(obj.gcd(x, y));
+        //System.out.println(obj.isprime(n));
+        //FactorsOfNum(n);
+        //System.out.println(obj.sumofdigits(n));
+        //System.out.println("Product of Digits : "+ productofdigit(n));
+        EvenOddCount(n);
+
+
 
     }
   }
